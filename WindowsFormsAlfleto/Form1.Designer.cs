6e8c1999cec93bcc3851675959ecc867b4b39fd5
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2017-11-15 13:38:44 -0300

    Ahi va trolo

---
+++ WindowsFormsAlfleto/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsAlfleto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dbTestDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dbTestDataSet = new WindowsFormsAlfleto.DbTestDataSet();
+            this.tablaTestTableAdapter = new WindowsFormsAlfleto.DbTestDataSetTableAdapters.TablaTestTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridView1.DataSource = this.dbTestDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "TextoRandom";
+            this.dataGridViewTextBoxColumn2.HeaderText = "TextoRandom";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dbTestDataSetBindingSource
+            // 
+            this.dbTestDataSetBindingSource.DataMember = "TablaTest";
+            this.dbTestDataSetBindingSource.DataSource = this.dbTestDataSet;
+            // 
+            // dbTestDataSet
+            // 
+            this.dbTestDataSet.DataSetName = "DbTestDataSet";
+            this.dbTestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tablaTestTableAdapter
+            // 
+            this.tablaTestTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ejecuta SP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dbTestDataSetBindingSource;
+        private DbTestDataSet dbTestDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textoRandomDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DbTestDataSetTableAdapters.TablaTestTableAdapter tablaTestTableAdapter;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsAlfleto/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //sql objects
+
+namespace WindowsFormsAlfleto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dbTestDataSet.TablaTest' Puede moverla o quitarla según sea necesario.
+            this.tablaTestTableAdapter.Fill(this.dbTestDataSet.TablaTest);
+
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            var myConn = new SqlConnection();
+            myConn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["WindowsFormsAlfleto.Properties.Settings.DbTestConnectionString"].ConnectionString;
+
+            using (myConn)
+            {
+                myConn.Open();
+                var myCommand = new SqlCommand("ProcedureTest", myConn); //Solo el nombre del procedimiento
+                myCommand.CommandType = CommandType.StoredProcedure;
+
+                var myReader = await myCommand.ExecuteReaderAsync();
+
+                while (await myReader.ReadAsync())
+                {
+                    //valor del campo TextoRandom
+                    var myTexto = myReader.GetFieldValue<string>(1);
+                    MessageBox.Show(myTexto);
+                }
+
+            }
+                
+
+
+        }
+    }
+}
